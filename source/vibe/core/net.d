1bba9d45b0a984d4b19eb61039951aaa9d2a2f24
Boris-Barboris <ismailsiege@gmail.com>
2018-03-22 20:47:14 +0000

    diffirentiate socket exception

--- source/vibe/core/net.d
+++ source/vibe/core/net.d
@@ -594,7 +594,7 @@ mixin(tracer);
 			m_context.readBuffer.popFrontN(n);
 			count -= n;
 			return count == 0;
-		});
+		}, ReadTimeoutException);
 	}
 
 	alias read = ConnectionStream.read;
@@ -604,12 +604,16 @@ mixin(tracer);
 mixin(tracer);
 		import std.algorithm.comparison : min;
 		if (!dst.length) return 0;
+		if (m_context.readBuffer.length >= dst.length) {
+			m_context.readBuffer.read(dst);
+			return dst.length;
+		}
 		size_t nbytes = 0;
 		m_context.readTimeout.loopWithTimeout!((remaining) {
 			if (m_context.readBuffer.length == 0) {
 				if (mode == IOMode.immediate || mode == IOMode.once && nbytes > 0)
 					return true;
-				enforce(waitForData(remaining), "Reached end of stream while reading data.");
+				enforce!SocketException(waitForData(remaining), "Reached end of stream while reading data.");
 			}
 			assert(m_context.readBuffer.length > 0);
 			auto l = min(dst.length, m_context.readBuffer.length);
@@ -617,7 +621,7 @@ mixin(tracer);
 			dst = dst[l .. $];
 			nbytes += l;
 			return dst.length == 0;
-		});
+		}, ReadTimeoutException);
 		return nbytes;
 	}
 
@@ -634,7 +638,7 @@ mixin(tracer);
 
 		switch (res[1]) {
 			default:
-				throw new Exception("Error writing data to socket.");
+				throw new SocketException("Error writing data to socket.");
 			case IOStatus.ok: break;
 			case IOStatus.disconnected: break;
 		}
@@ -682,14 +686,14 @@ mixin(tracer);
 
 //mixin validateConnectionStream!TCPConnection;
 
-private void loopWithTimeout(alias LoopBody, ExceptionType = Exception)(Duration timeout)
+private void loopWithTimeout(alias LoopBody, ExceptionType = Exception)(Duration timeout,
+	immutable string timeoutMsg = "Operation timed out.")
 {
-	import core.time : seconds;
-	import std.datetime : Clock, SysTime, UTC;
+	import core.time : seconds, MonoTime;
 
-	SysTime now;
+	MonoTime now;
 	if (timeout != Duration.max)
-		now = Clock.currTime(UTC());
+		now = MonoTime.currTime();
 
 	do {
 		if (LoopBody(timeout))
@@ -697,12 +701,12 @@ private void loopWithTimeout(alias LoopBody, ExceptionType = Exception)(Duration
 
 		if (timeout != Duration.max) {
 			auto prev = now;
-			now = Clock.currTime(UTC());
+			now = MonoTime.currTime();
 			if (now > prev) timeout -= now - prev;
 		}
 	} while (timeout > 0.seconds);
 
-	throw new ExceptionType("Operation timed out.");
+	throw new ExceptionType(timeoutMsg);
 }
 
 
@@ -916,7 +920,7 @@ enum TCPListenOptions {
 	/// Disables automatic closing of the connection when the connection callback exits
 	disableAutoClose = 1<<1,
 	/** Enable port reuse on linux kernel version >=3.9, do nothing on other OS
-	    Does not affect libasync driver because it is always enabled by libasync.
+		Does not affect libasync driver because it is always enabled by libasync.
 	*/
 	reusePort = 1<<2,
 }
@@ -940,3 +944,34 @@ private pure nothrow {
 }
 
 private enum tracer = "";
+
+
+private mixin template ExceptionConstructors()
+{
+	@safe pure nothrow this(string message,
+							Throwable next,
+							string file =__FILE__,
+							size_t line = __LINE__)
+	{
+		super(message, next, file, line);
+	}
+
+	@safe pure nothrow this(string message,
+							string file =__FILE__,
+							size_t line = __LINE__,
+							Throwable next = null)
+	{
+		super(message, file, line, next);
+	}
+}
+
+class SocketException: Exception
+{
+	mixin ExceptionConstructors;
+}
+
+/// Thrown by TCPConnection read-alike operations when timeout is reached
+class ReadTimeoutException: SocketException
+{
+	mixin ExceptionConstructors;
+}
